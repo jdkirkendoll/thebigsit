270ee23010e3b1b5d32fda7013e9f36ed802c3a9
jdkirkendoll <47092959+jdkirkendoll@users.noreply.github.com>
2019-11-02 21:03:00 -0500

    Add files via upload

---
+++ conf.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'APP_KEY': 'dj0yJmk9ZW9PWmFSVGxJRllWJmQ9WVdrOWNETmxaMnhWTjJjbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTU0',
+  'APP_SECRET': '961d38c3d371398da486a9dce04f2bcd62df24c0',
+  'APP_URL': 'http://127.0.0.1'
+};
